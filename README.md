52fd68a6eb6ca71b90e11a618ac4c184efad665b
prahladinala <prahlad2018@gmail.com>
2021-04-15 23:38:18 +0530

    Files added with build Directory

---
+++ README.md
@@ -0,0 +1,1 @@
+Color Palette using React
---
+++ build/precache-manifest.cec866b90bbf49517a951171d20a75ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3455c3b9faec3216786bb881b968172",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4663a1a8f5b40da1f1c8",
+    "url": "./static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "./static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "./static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4663a1a8f5b40da1f1c8",
+    "url": "./static/js/main.125abb68.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  }
+]);
